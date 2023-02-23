bc4a7827c5ae22adf97b3d668897a6be07c3e644
Mauricio Chasi Guerrero <mauriciochasi@gmail.com>
2023-02-23 12:23:41 +0100

    python

---
+++ alumno.py
@@ -0,0 +1,36 @@
+
+class Alumno:
+    # inicializamos los atributos
+    def inicializar(self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
+
+
+    # función para imprimir los datos
+    def imprimir(self):
+        print("Nombre: ",self.nombre)
+        print("Nota: ",self.nota)
+
+
+    # función para obtener el resultado
+    def resultado(self):
+
+        if self.nota < 5:
+            print("El alumno ha suspendido")
+        else:
+            print("El alumno ha aprobado")
+
+
+# creamos los nuevos objetos
+alumno1=Alumno()
+alumno2=Alumno()
+
+# inicializamos los atributos
+alumno1.inicializar("Borja",4)
+alumno2.inicializar("Alavaro",9)
+
+# imprimimos los datos y resultados de cada alumno
+alumno1.imprimir()
+alumno1.resultado()
+alumno2.imprimir()
+alumno2.resultado()
